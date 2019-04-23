3ff186079ca926d4d11af6b1978942e198e886a5
林仁鴻 <appleken51688@gmail.com>
2019-04-24 00:58:32 +0800

    Initial Commit

---
+++ AES(RenHong)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AES(RenHong)
+//
+//  Created by 林仁鴻 on 2019/4/24.
+//  Copyright © 2019 林仁鴻. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
